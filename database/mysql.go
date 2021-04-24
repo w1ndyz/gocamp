4a3f59cfc9716e7310fbb55518c16cc2ee1e5f5d
w1ndyz <fzzf0618@126.com>
2021-04-25 06:28:19 +0800

    first commit

--- database/mysql.go
+++ database/mysql.go
@@ -11,7 +11,7 @@ var DB *sql.DB
 
 func Setup() {
 	var err error
-	dsn := fmt.Sprint("root:Woailm0618.@tcp(localhost:3306)/business-card?charset=utf8mb4&parseTime=True&loc=Local&readTimeout=5s")
+	dsn := fmt.Sprint("root:root@tcp(localhost:3306)/business-card?charset=utf8mb4&parseTime=True&loc=Local&readTimeout=5s")
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Panic("set up failed")
